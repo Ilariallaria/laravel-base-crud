45a3bfdba581efa2b9039b694a4a5edc7e81455a
Ilariallaria <hilary.lion@gmail.com>
2022-08-30 20:51:52 +0200

    first commit

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics_array = config('comics');
+        
+        foreach ($comics_array as $comics) {
+        $new_comics = new Comic();
+        $new_comics->title = $comics['title'];
+        $new_comics->description = $comics['description'];
+        $new_comics->thumb = $comics['thumb'];
+        $new_comics->price = $comics['price'];
+        $new_comics->series = $comics['series'];
+        $new_comics->sale_date = $comics['sale_date'];
+        $new_comics->type = $comics['type'];
+        $new_comics->save();
+        }
+    }
+    
+}
