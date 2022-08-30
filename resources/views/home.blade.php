7259c6e4ab228b4773d3dee2b702ba08a5b4e6d5
Ilariallaria <hilary.lion@gmail.com>
2022-08-31 00:21:20 +0200

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>Homepage</h1>
+    <h1>COMICS HOMEPAGE</h1>
+    <div>
+        Check <a href="{{ route('comics.index') }}"> Comics list</a> 
+    </div>
+    <br>
+    <div>or</div>
+    <br>
+    <div>
+        <a href="{{route('comics.create')}}">create </a>your hown comic!
+    </div>
+    <br>
+    <div>ENJOY!!</div>
 @endsection
